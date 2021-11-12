40ac1d07c4a3ed112c172c9288d33656d14bff90
rhiannonmay <93537261+rhiannonmay@users.noreply.github.com>
2021-11-12 04:22:48 -0700

    Add files via upload

---
+++ app/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField 
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User 
+
+
+class LoginForm(FlaskForm):
+        username = StringField('Username', validators=[DataRequired()])
+        password = PasswordField('Password', validators=[DataRequired()])
+        remember_me = BooleanField ('Remember Me')
+        submit = SubmitField('Sing In')
+
+class RegistrationForm(FlaskForm):
+      username = StringField('Username', validators=[DataRequired()])
+      email = StringField('Email', validators=[DataRequired(), Email()])
+      password = PasswordField('Password', validators=[DataRequired()])
+      password2 = PasswordField(
+              'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+      submit = SubmitField('Register')
+      
+def validate_username(self, username):
+        user = User.query.filter_buy(username-username.data).first()
+        if user is not None:
+                raise ValidationError('This username is unavailable')
+
+
+def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+                raise ValidationError('There is already an account registered under this email')
